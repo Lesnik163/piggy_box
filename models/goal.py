11782a0da6131a7f77eb3c1f7a2f45f2e4c99cc8
Lesnik163 <dmitry.lyubich@mvideo.ru>
2026-09-02 10:25:28 +0300

    feature: add class Goal

--- models/goal.py
+++ models/goal.py
@@ -1,23 +1,14 @@
 class Goal:
-  def __init__(self, name, target_amount, status, category):
-    self.name = name
-    self.target_amount = target_amount
-    self.current_balance = 0
-    self.status = status
-    self.category = category
+    def __init__(self, name, target_amount, category, status="активна"):
+        self.name = name
+        self.target_amount = target_amount
+        self.current_balance = 0
+        self.status = status
+        self.category = category
 
-  def __str__(self):
-    percent = self.current_balance / self.target_amount * 100
-    return (
-        f"{self.name}: {self.current_balance:.0f} из {self.target_amount:.0f} "
-        f"({percent:.1f}%) — {self.status}"
-    )
-    
-goals = []
-
-def add_goal(name, target_amount, status, category):
-  goal = Goal(name, target_amount, status, category)
-  goals.append(goal)
-  print(f"Цель {goal} добавлена")
-  return goal
-
+    def __str__(self):
+        percent = self.current_balance / self.target_amount * 100
+        return (
+            f"{self.name}: {self.current_balance:.0f} из {self.target_amount:.0f} "
+            f"({percent:.1f}%) — {self.status}"
+        )
